0a2193cce4c55573ab91446ab0d0d76ba09475a7
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-10-30 13:47:11 -0700

    Add btn for search

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -2,14 +2,13 @@ export default function Games({ games }) {
   if (!games) return null;
 
   // Collect all unique games in this Pokémon’s data
-  const pokemonGames = new Set(games.map(g => g.version.name));
+  const pokemonGames = new Set(games.map((g) => g.version.name));
 
   // Sort alphabetically for display
   const allGames = Array.from(pokemonGames).sort();
 
   // ‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️‼️
-// NOT WORKING ATM!!! Should be highlighting in green if the pokemon is in that game but right now is not doing that :(
-
+  // NOT WORKING ATM!!! Should be highlighting in green if the pokemon is in that game but right now is not doing that :(
 
   return (
     <div>
--- src/components/PokemonStats.jsx
+++ src/components/PokemonStats.jsx
@@ -1,24 +1,49 @@
-import Item from "./Item.jsx"
+import Item from "./Item.jsx";
 
 export default function PokemonStats({ data, canEvolve }) {
-    if (!data) return null
+  if (!data) return null;
 
-    return (
-        <div className="PokeStats">
-          
-            <Item label="Height" value={`${data.height / 10} m`} />
-            <Item label="Weight" value={`${data.weight} g`} />
-            <Item label="Type(s)" value={data.types.map(t => t.type.name).join("/")} />
+  console.log(data);
 
-            <Item label="HP" value={data.stats.find(s => s.stat.name === "hp")?.base_stat} />
-            <Item label="Attack" value={data.stats.find(s => s.stat.name === "attack")?.base_stat} />
-            <Item label="Defense" value={data.stats.find(s => s.stat.name === "defense")?.base_stat} />
-            <Item label="Sp. Attack" value={data.stats.find(s => s.stat.name === "special-attack")?.base_stat} />
-            <Item label="Sp. Defense" value={data.stats.find(s => s.stat.name === "special-defense")?.base_stat} />
-            <Item label="Speed" value={data.stats.find(s => s.stat.name === "speed")?.base_stat} />
+  return (
+    <div className="PokeStats">
+      <Item label="Height" value={`${data.height / 10} m`} />
+      <Item label="Weight" value={`${data.weight} g`} />
+      <Item
+        label="Type(s)"
+        value={data.types.map((t) => t.type.name).join("/")}
+      />
 
-            <Item label="Evolves" value={canEvolve ? "Yes" : "No"} />
+      <Item
+        label="HP"
+        value={data.stats.find((s) => s.stat.name === "hp")?.base_stat}
+      />
+      <Item
+        label="Attack"
+        value={data.stats.find((s) => s.stat.name === "attack")?.base_stat}
+      />
+      <Item
+        label="Defense"
+        value={data.stats.find((s) => s.stat.name === "defense")?.base_stat}
+      />
+      <Item
+        label="Sp. Attack"
+        value={
+          data.stats.find((s) => s.stat.name === "special-attack")?.base_stat
+        }
+      />
+      <Item
+        label="Sp. Defense"
+        value={
+          data.stats.find((s) => s.stat.name === "special-defense")?.base_stat
+        }
+      />
+      <Item
+        label="Speed"
+        value={data.stats.find((s) => s.stat.name === "speed")?.base_stat}
+      />
 
-        </div>
-    )
+      <Item label="Evolves" value={canEvolve ? "Yes" : "No"} />
+    </div>
+  );
 }
